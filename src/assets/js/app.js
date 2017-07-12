fc26815ff3bd90c64581e11c26ca91dbfd75862f
Mark McMurray <requestingmark@gmail.com>
2017-07-12 16:01:05 -0500

    added skip link and modified works

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,3 +5,8 @@ $('.about-menu__button').click(function() {
 	$('.about-menu__item').removeClass('about-menu__item--bottom');
 	$(this).parent().addClass('about-menu__item--bottom');
 });
+
+/*$(document).on('open.zf.reveal', '[data-reveal]', function() {
+  console.log('This works');
+  var foo = new Foundation.Orbit($('.orbit'));
+});*/
